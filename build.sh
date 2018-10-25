b4c3675f350d7592153af1b315f36f1322cd6335
Francis Lim <thyeem@gmail.com>
2018-10-25 18:27:06 +0900

    removed .ssh related in build.ts

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash 
 
-sudo chmod 700 .ssh 
-sudo chmod 600 .ssh/id_rsa 
-sudo chmod 644 .ssh/id_rsa.pub .ssh/config 
-
 sudo docker build -t freehycon . 
 
 dangling=$(docker images -q -f dangling=true | wc -l)
